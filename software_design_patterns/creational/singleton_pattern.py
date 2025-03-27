662b52d425e1b92a35b03cce476e915a6e862641
eduairet <hola@eduairet.com>
2025-03-26 21:53:02 -0600

    Test singleton

--- software_design_patterns/creational/singleton_pattern.py
+++ software_design_patterns/creational/singleton_pattern.py
@@ -1,1 +1,62 @@
-# TODO Add singleton pattern example
+from hashlib import sha256
+
+
+def singleton(cls):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+
+class Block:
+    def __init__(
+        self,
+        timestamp: int,
+        transactions: list[Transaction],
+        previous_hash: str,
+        nonce: int = 0,
+    ):
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self) -> str:
+        return sha256(
+            str(self.timestamp).encode()
+            + str(self.transactions).encode()
+            + str(self.previous_hash).encode()
+            + str(self.nonce).encode()
+        ).hexdigest()
+
+
+@singleton
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.nonce = 1
+
+    def create_genesis_block(self) -> Block:
+        return Block(0, [], "0", 0)
+
+    def get_latest_block(self) -> Block:
+        return self.chain[-1]
+
+    def add_block(self, new_block: Block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        new_block.nonce = self.nonce
+        self.chain.append(new_block)
+        self.nonce += 1
--- tests/test_singleton_pattern.py
+++ tests/test_singleton_pattern.py
@@ -1,3 +1,27 @@
 from software_design_patterns.creational.singleton_pattern import *
 
-# TODO Add tests for Singleton Pattern
+GENESIS_BLOCK_HASH = "ec596eea922c4f9b003eb27d2ea8744d2d43e09401cb17f597906abea72a89ca"
+
+
+def test_singleton_blockchain():
+    blockchain = Blockchain()
+
+    assert len(blockchain.chain) == 1
+    assert blockchain.get_latest_block().hash == GENESIS_BLOCK_HASH
+
+    transaction1 = Transaction("Alice", "Bob", 10)
+    block1 = Block(1, [transaction1], GENESIS_BLOCK_HASH)
+    blockchain.add_block(block1)
+
+    assert len(blockchain.chain) == 2
+    assert blockchain.get_latest_block().hash != GENESIS_BLOCK_HASH
+    assert blockchain.get_latest_block().previous_hash == GENESIS_BLOCK_HASH
+    assert blockchain.get_latest_block().transactions == [transaction1]
+    assert blockchain.get_latest_block().nonce == 1
+
+    blockchain_2 = Blockchain()
+
+    assert blockchain is blockchain_2 is Blockchain()
+    assert (
+        len(blockchain.chain) == len(blockchain_2.chain) == len(Blockchain().chain) == 2
+    )

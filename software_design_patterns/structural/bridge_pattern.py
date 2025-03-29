43e4bbfc263d2540dc7b94a93764a1d7959736d2
eduairet <hola@eduairet.com>
2025-03-28 22:21:07 -0600

    Bridge example

--- software_design_patterns/structural/bridge_pattern.py
+++ software_design_patterns/structural/bridge_pattern.py
@@ -1,1 +1,46 @@
-# TODO Add example
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Cooker(ABC):
+    @abstractmethod
+    def cook(self, taco: "Taco") -> str:
+        pass
+
+
+class CookerFried(Cooker):
+    def cook(self, taco: "Taco") -> str:
+        return f"Frying {taco.name} taco{f' then add {taco.toppings_str}' if taco.toppings else ''}."
+
+
+class CookerGrilled(Cooker):
+    def cook(self, taco: "Taco") -> str:
+        return f"Grilling {taco.name} taco{f' then add {taco.toppings_str}' if taco.toppings else ''}."
+
+
+class Taco(ABC):
+    def __init__(self, name: str, cooker: Cooker):
+        self.name = name
+        self.cooker = cooker
+        self.toppings: List[str] = []
+
+    def add_toppings(self, *toppings: str) -> "Taco":
+        self.toppings.extend(toppings)
+        return self
+
+    @property
+    def toppings_str(self) -> str:
+        return ", ".join(self.toppings)
+
+    def __str__(self) -> str:
+        return self.cooker.cook(self)
+
+
+class TacoCarnitas(Taco):
+    def __init__(self):
+        super().__init__("carnitas", CookerFried())
+
+
+class TacoAsada(Taco):
+    def __init__(self):
+        super().__init__("asada", CookerGrilled())
--- tests/test_bridge_pattern.py
+++ tests/test_bridge_pattern.py
@@ -1,1 +1,14 @@
 from software_design_patterns.structural.bridge_pattern import *
+
+
+def test_bridge_taco():
+    TACO_CARNITAS_EXPECTED = "Frying carnitas taco then add cilantro, guacamole."
+    TACO_ASADA_EXPECTED = (
+        "Grilling asada taco then add chiles toreados, cebolla rostizada."
+    )
+
+    taco = TacoCarnitas().add_toppings("cilantro", "guacamole")
+    assert str(taco) == TACO_CARNITAS_EXPECTED
+
+    taco = TacoAsada().add_toppings("chiles toreados", "cebolla rostizada")
+    assert str(taco) == TACO_ASADA_EXPECTED

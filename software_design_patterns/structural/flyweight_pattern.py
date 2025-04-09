75cf2faf16764b6e56bab650351b22c0b378ce49
eduairet <hola@eduairet.com>
2025-04-08 23:19:26 -0600

    Flyweight exercise

---
+++ software_design_patterns/structural/flyweight_pattern.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+
+class MeatType(Enum):
+    BEEF = "Beef"
+    PORK = "Pork"
+
+
+class MeatCuts:
+    _instances = {}
+
+    @classmethod
+    def get_instance(cls, name: str, meat_type: MeatType):
+        if meat_type not in cls._instances:
+            cls._instances[meat_type] = {}
+
+        if name not in cls._instances[meat_type]:
+            cls._instances[meat_type][name] = super().__new__(cls)
+
+        return cls._instances[meat_type][name]
+
+    def __new__(cls, name: str, meat_type: MeatType):
+        return cls.get_instance(name, meat_type)
+
+
+class Cut:
+    def __init__(self, name: str, meat_type: MeatType, meat_cuts: MeatCuts):
+        self.name = name
+        self.meat_type = meat_type
+        self.meat_cuts = meat_cuts
+
+    def __str__(self):
+        cut = self.meat_cuts.get_instance(self.name, self.meat_type)
+        return f"Cut: {self.name}, Type: {self.meat_type.value}"
+
+
+class ButcherShop:
+    def __init__(self):
+        self.cuts = []
+
+    def add_cut(self, name: str, meat_type: MeatType):
+        meat_cuts = MeatCuts.get_instance(name, meat_type)
+        cut = Cut(name, meat_type, meat_cuts)
+        self.cuts.append(cut)
+        return self
+
+    def __str__(self):
+        return "\n".join(str(cut) for cut in self.cuts)
--- tests/test_flyweight_pattern.py
+++ tests/test_flyweight_pattern.py
@@ -1,1 +1,18 @@
 from software_design_patterns.structural.flyweight_pattern import *
+
+
+def test_flyweight_meat_cuts():
+    butcher_shop = ButcherShop()
+
+    butcher_shop.add_cut("Ribeye", MeatType.BEEF).add_cut(
+        "Tenderloin", MeatType.BEEF
+    ).add_cut("Pork Chop", MeatType.PORK).add_cut("Pork Belly", MeatType.PORK)
+
+    EXPECTED_OUTPUT = (
+        "Cut: Ribeye, Type: Beef\n"
+        "Cut: Tenderloin, Type: Beef\n"
+        "Cut: Pork Chop, Type: Pork\n"
+        "Cut: Pork Belly, Type: Pork"
+    )
+
+    assert str(butcher_shop) == EXPECTED_OUTPUT

82c6930160ee5bfd82d74e3bb81f41e08ec3ce40
eduairet <hola@eduairet.com>
2025-04-21 22:01:52 -0600

    Interpreter example

--- software_design_patterns/behavioral/interpreter_pattern.py
+++ software_design_patterns/behavioral/interpreter_pattern.py
@@ -1,1 +1,69 @@
-# Interpreter Pattern
+from abc import ABC, abstractmethod
+from enum import Enum
+import re
+
+
+class Language(Enum):
+    RUST = "Rust"
+    PYTHON = "Python"
+
+
+# This is just a sample of translation rules, in real-world applications it's better to use a battle-tested library
+TRANSLATION_RULES = {
+    (Language.RUST, Language.PYTHON): [
+        (r"fn (\w+)\((.*?)\) -> (.*?) {([^}]*)}", r"def \1(\2) -> \3:\4"),
+        (r"let (\w+) = (.*?);", r"\1 = \2"),
+        (r"return (.*?);", r"return \1"),
+        (r"(i32|i64|u32|u64)", "int"),
+    ],
+    (Language.PYTHON, Language.RUST): [
+        (r"def (\w+)\((.*?)\) -> (.*?):([^}]*)", r"fn \1(\2) -> \3 {\4}"),
+        (r"(\w+) = ([^\n]+)\n", r"let \1 = \2;\n"),
+        (r"return ([^\n]+)\n", r"return \1;\n"),
+        (r"(int)", "i32"),
+    ],
+}
+
+
+class Context:
+    def __init__(self, language_from: Language, language_to: Language):
+        self.language_from = language_from
+        self.language_to = language_to
+
+
+class Expression(ABC):
+    def __init__(self, context: Context):
+        self.context = context
+
+    @abstractmethod
+    def interpret(self, code: str) -> str:
+        pass
+
+
+class GeneralInterpreter(Expression):
+    def __init__(self, context: Context):
+        super().__init__(context)
+        self.rules = TRANSLATION_RULES.get(
+            (context.language_from, context.language_to), []
+        )
+
+    def interpret(self, code: str) -> str:
+        if not self.rules:
+            raise ValueError(
+                f"No translation rules for {self.context.language_from} to {self.context.language_to}"
+            )
+        for pattern, replacement in self.rules:
+            code = re.sub(pattern, replacement, code)
+        return code
+
+
+class RustToPythonInterpreter(GeneralInterpreter):
+    def __init__(self):
+        context = Context(Language.RUST, Language.PYTHON)
+        super().__init__(context)
+
+
+class PythonToRustInterpreter(GeneralInterpreter):
+    def __init__(self):
+        context = Context(Language.PYTHON, Language.RUST)
+        super().__init__(context)
--- tests/test_interpreter_pattern.py
+++ tests/test_interpreter_pattern.py
@@ -1,1 +1,24 @@
+import pytest
 from software_design_patterns.behavioral.interpreter_pattern import *
+
+
+def test_interpreter_rust_to_python():
+    RUST_CODE = """fn add(x: i32, y: i32) -> i32 {
+    let result = x + y;
+    return result;
+}"""
+    EXPECTED_PYTHON_CODE = """def add(x: int, y: int) -> int:
+    result = x + y
+    return result
+"""
+    rs_to_py_interpreter = RustToPythonInterpreter()
+    python_code = rs_to_py_interpreter.interpret(RUST_CODE)
+    assert python_code == EXPECTED_PYTHON_CODE
+
+    local_scope = {}
+    exec(python_code, {}, local_scope)
+    assert local_scope["add"](2, 3) == 5
+
+    py_to_rs_interpreter = PythonToRustInterpreter()
+    rust_code_converted_back = py_to_rs_interpreter.interpret(python_code)
+    assert rust_code_converted_back == RUST_CODE

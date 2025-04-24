989b3ff58431df65926d6587f0b5f2bfaa81238c
eduairet <hola@eduairet.com>
2025-04-23 23:07:21 -0600

    Memento exercise

--- software_design_patterns/behavioral/memento_pattern.py
+++ software_design_patterns/behavioral/memento_pattern.py
@@ -1,1 +1,48 @@
-# Memento Pattern
+class Memento:
+    def __init__(self, state):
+        self._state = state
+
+    def get_state(self):
+        return self._state
+
+
+class Poem:
+    def __init__(self, text: str = ""):
+        self._text = text
+        self.changes = [Memento(self.text)]
+        self._current = 0
+
+    @property
+    def text(self):
+        return self._text
+
+    def set_text(self, text: str):
+        self._text = text
+        m = Memento(self._text)
+        self.changes.append(m)
+        self._current += 1
+        return m
+
+    def restore(self, memento: Memento):
+        self._text = memento.get_state()
+        self.changes.append(memento)
+        self._current = len(self.changes) - 1
+
+    def undo(self):
+        if self._current > 0:
+            self._current -= 1
+            m = self.changes[self._current]
+            self._text = m.get_state()
+            return m
+        return None
+
+    def redo(self):
+        if self._current < len(self.changes) - 1:
+            self._current += 1
+            m = self.changes[self._current]
+            self._text = m.get_state()
+            return m
+        return None
+
+    def __str__(self):
+        return self._text
--- tests/test_memento_pattern.py
+++ tests/test_memento_pattern.py
@@ -1,1 +1,30 @@
 from software_design_patterns.behavioral.memento_pattern import *
+
+
+def test_memento_poem():
+    VERSE_1 = "Que putas haces con mi corazón"
+    VERSE_2 = "Deshidratado en el infierno"
+    VERSE_3 = "De una vida sin anhelo"
+
+    poem = Poem(VERSE_1)
+    assert poem.text == VERSE_1
+
+    memento1 = poem.set_text(VERSE_2)
+    assert poem.text == VERSE_2
+
+    memento2 = poem.set_text(VERSE_3)
+    assert poem.text == VERSE_3
+
+    poem.restore(memento1)
+    assert poem.text == VERSE_2
+
+    poem.restore(memento2)
+    assert poem.text == VERSE_3
+
+    memento3 = poem.undo()
+    assert poem.text == VERSE_2
+    assert memento3.get_state() == VERSE_2
+
+    memento4 = poem.redo()
+    assert poem.text == VERSE_3
+    assert memento4.get_state() == VERSE_3

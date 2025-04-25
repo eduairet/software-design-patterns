44546c2950474df790b837c9b46f71751a3b6135
eduairet <hola@eduairet.com>
2025-04-25 00:01:03 -0600

    Observer exercise

--- software_design_patterns/behavioral/observer_pattern.py
+++ software_design_patterns/behavioral/observer_pattern.py
@@ -1,1 +1,86 @@
-# Observer Pattern
+import time
+
+END_OF_QUEUE_LOG = "No more songs in the queue."
+NOW_PLAYING_LOG = lambda song: (
+    f"Now playing: {song.title} by {song.artist} | Time: {song.current_time}/{song.time}"
+)
+NEXT_SONG_LOG = lambda song: (
+    f"Next song: {song.title} by {song.artist}" if song else END_OF_QUEUE_LOG
+)
+
+
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for item in self:
+            item(*args, **kwargs)
+
+
+class PropertyObservable:
+    def __init__(self):
+        self.property_changed = Event()
+        return self
+
+
+class Song:
+    def __init__(self, title: str, artist: str, time: int):
+        self.title = title
+        self.artist = artist
+        self.time = time
+        self.current_time = 0
+
+
+class Radio(PropertyObservable):
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+        self._now_playing = None
+        self.log = []
+
+    def now_playing_msg(self):
+        self.property_changed("now_playing", self._now_playing)
+        self.log.append(NOW_PLAYING_LOG(self._now_playing))
+
+    def next_song_msg(self):
+        self.property_changed("next_song", self._now_playing)
+        self.log.append(NEXT_SONG_LOG(self._now_playing))
+
+    def end_of_queue_msg(self):
+        self.property_changed("end_of_queue")
+        self.log.append(END_OF_QUEUE_LOG)
+
+    def add_song(self, song: Song):
+        self.queue.append(song)
+        return self
+
+    def play(self):
+        if not self.queue:
+            self.end_of_queue_msg()
+            return
+
+        while self.queue:
+            self._now_playing = self.queue[0]
+
+            while not self.next_song:
+                time.sleep(1)
+                self._now_playing.current_time += 1
+                self.now_playing_msg()
+
+            self.queue.pop(0)
+            if self.queue:
+                self._now_playing = self.queue[0]
+                self.next_song_msg()
+            else:
+                self.end_of_queue_msg()
+
+    @property
+    def next_song(self):
+        return self._now_playing.current_time >= self._now_playing.time
+
+
+def song_changed(event: str, song: Song = None):
+    if event == "next_song":
+        print(NEXT_SONG_LOG(song))
+    elif event == "now_playing":
+        print(NOW_PLAYING_LOG(song))
+    elif event == "end_of_queue":
+        print(END_OF_QUEUE_LOG)
--- tests/test_observer_pattern.py
+++ tests/test_observer_pattern.py
@@ -1,1 +1,27 @@
 from software_design_patterns.behavioral.observer_pattern import *
+
+
+def test_observer_radio():
+    EXPECTED_LOG = [
+        "Now playing: Laisse-moi by KeBlack | Time: 1/2",
+        "Now playing: Laisse-moi by KeBlack | Time: 2/2",
+        "Next song: Chona by Los Tucanes de Tijuana",
+        "Now playing: Chona by Los Tucanes de Tijuana | Time: 1/3",
+        "Now playing: Chona by Los Tucanes de Tijuana | Time: 2/3",
+        "Now playing: Chona by Los Tucanes de Tijuana | Time: 3/3",
+        "No more songs in the queue.",
+    ]
+
+    laisse_moi = Song("Laisse-moi", "KeBlack", 2)
+    chona = Song("Chona", "Los Tucanes de Tijuana", 3)
+
+    radio = Radio().add_song(laisse_moi).add_song(chona)
+    radio.property_changed.append(song_changed)
+    print("\nPlaying radio...")
+    radio.play()
+
+    assert radio.log == EXPECTED_LOG
+
+    radio.property_changed.remove(song_changed)
+
+    assert len(radio.property_changed) == 0

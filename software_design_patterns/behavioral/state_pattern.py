ee28d907e2d8b33dbd8d85d0fa5db0b6408f29a5
eduairet <hola@eduairet.com>
2025-04-28 21:56:53 -0600

    State exercise

--- software_design_patterns/behavioral/state_pattern.py
+++ software_design_patterns/behavioral/state_pattern.py
@@ -1,1 +1,73 @@
-# State Pattern
+from enum import Enum, auto
+from random import choice
+
+
+class Player:
+    def __init__(self, name: str, balance: int):
+        self.name = name
+        self.balance = balance
+
+
+class State(Enum):
+    START = auto()
+    PLAYING = auto()
+    END = auto()
+
+
+class Coin(Enum):
+    HEADS = auto()
+    TAILS = auto()
+
+
+class HeadsOrTails:
+    def __init__(self, player_heads: Player, player_tails: Player, bet: int):
+        self.player_heads = player_heads
+        self.player_tails = player_tails
+        self.state = State.START
+        self.bet = bet
+        self.winner = None
+
+    def play(self):
+        if self.state == State.START:
+            print(
+                f"{self.player_heads.name} and {self.player_tails.name} are ready to play!"
+            )
+            self.state = State.PLAYING
+
+        while self.state == State.PLAYING:
+            self._play_round()
+
+    def _play_round(self):
+        player_heads_bet = min(self.bet, self.player_heads.balance)
+        player_tails_bet = min(self.bet, self.player_tails.balance)
+
+        round_result = choice([Coin.HEADS, Coin.TAILS])
+        print(f"Round result: {round_result.name} |", end=" ")
+
+        if round_result == Coin.HEADS:
+            self._update_balances(player_heads_bet, -player_tails_bet)
+        else:
+            self._update_balances(-player_heads_bet, player_tails_bet)
+
+        self._check_game_over()
+
+    def _update_balances(self, heads_change: int, tails_change: int):
+        self.player_heads.balance += heads_change
+        self.player_tails.balance += tails_change
+
+    def _check_game_over(self):
+        if self.player_heads.balance == 0 or self.player_tails.balance == 0:
+            self.winner = (
+                self.player_heads
+                if self.player_heads.balance > 0
+                else self.player_tails
+            )
+            print(
+                f"Game over! Winner is: {self.winner.name} with balance {self.winner.balance}"
+            )
+            self.state = State.END
+        else:
+            print(
+                f"{self.player_heads.name} balance: {self.player_heads.balance}, "
+                f"{self.player_tails.name} balance: {self.player_tails.balance}"
+            )
--- tests/test_state_pattern.py
+++ tests/test_state_pattern.py
@@ -1,1 +1,14 @@
 from software_design_patterns.behavioral.state_pattern import *
+
+
+def test_state_heads_or_tails():
+    player_heads = Player("Alice", 20)
+    player_tails = Player("Bob", 20)
+    game = HeadsOrTails(player_heads, player_tails, 10)
+
+    print("\n")
+    game.play()
+
+    assert game.winner is not None
+    assert game.winner.balance == player_heads.balance + player_tails.balance
+    assert game.state == State.END

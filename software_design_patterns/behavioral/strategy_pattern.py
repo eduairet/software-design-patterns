1277430d41cea60f7ca68e94da0a02d1240dbbde
eduairet <hola@eduairet.com>
2025-04-29 20:57:21 -0600

    Strategy exercise

--- software_design_patterns/behavioral/strategy_pattern.py
+++ software_design_patterns/behavioral/strategy_pattern.py
@@ -1,1 +1,71 @@
-# Strategy Pattern
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class WineType(Enum):
+    RED = "Red"
+    WHITE = "White"
+    SPARKLING = "Sparkling"
+
+
+class WineCupType(Enum):
+    STANDARD_RED = "Standard Red"
+    BLANC = "Blanc"
+    FLUTE = "Flute"
+
+
+class WineStrategy(ABC):
+    @abstractmethod
+    def temp(self) -> str:
+        pass
+
+    def oxygen(self) -> str:
+        return ""
+
+    def serve(self, wine_type: WineType) -> str:
+        cup_mapping = {
+            WineType.RED: WineCupType.STANDARD_RED,
+            WineType.WHITE: WineCupType.BLANC,
+            WineType.SPARKLING: WineCupType.FLUTE,
+        }
+        cup = cup_mapping[wine_type]
+        return f"Serve {wine_type.value} wine in a {cup.value} cup"
+
+
+class RedWineStrategy(WineStrategy):
+    def temp(self) -> str:
+        return "Serve at 16-18°C"
+
+    def oxygen(self) -> str:
+        return "Let it breathe for 30 minutes"
+
+
+class WhiteWineStrategy(WineStrategy):
+    def temp(self) -> str:
+        return "Serve at 8-10°C"
+
+
+class SparklingWineStrategy(WineStrategy):
+    def temp(self) -> str:
+        return "Serve at 6-8°C"
+
+
+class WineServer:
+    def __init__(self, wine_type: WineType):
+        self.wine_type = wine_type
+        self.strategy = self.get_strategy(wine_type)
+
+    def get_strategy(self, wine_type: WineType) -> WineStrategy:
+        strategy_mapping = {
+            WineType.RED: RedWineStrategy(),
+            WineType.WHITE: WhiteWineStrategy(),
+            WineType.SPARKLING: SparklingWineStrategy(),
+        }
+        return strategy_mapping.get(wine_type, ValueError("Unknown wine type"))
+
+    def execute_strategy(self) -> str:
+        temp = self.strategy.temp()
+        oxygen = self.strategy.oxygen()
+        serve = self.strategy.serve(self.wine_type)
+        instructions = [i for i in [temp, oxygen, serve] if i]
+        return "\n".join(instructions)
--- tests/test_strategy_pattern.py
+++ tests/test_strategy_pattern.py
@@ -1,1 +1,31 @@
 from software_design_patterns.behavioral.strategy_pattern import *
+
+
+def test_strategy_red_wine():
+    wine_server = WineServer(WineType.RED)
+    assert wine_server.execute_strategy() == (
+        "Serve at 16-18°C\n"
+        "Let it breathe for 30 minutes\n"
+        "Serve Red wine in a Standard Red cup"
+    )
+
+
+def test_strategy_white_wine():
+    wine_server = WineServer(WineType.WHITE)
+    assert wine_server.execute_strategy() == (
+        "Serve at 8-10°C\n" "Serve White wine in a Blanc cup"
+    )
+
+
+def test_strategy_sparkling_wine():
+    wine_server = WineServer(WineType.SPARKLING)
+    assert wine_server.execute_strategy() == (
+        "Serve at 6-8°C\n" "Serve Sparkling wine in a Flute cup"
+    )
+
+
+def test_strategy_invalid_wine_type():
+    try:
+        WineServer("INVALID")
+    except ValueError as e:
+        assert str(e) == "Unknown wine type"
